b99b14c99ab0e11a91ed407f83e6e887a413d6b9
Stanley Metray <stanleymetray@gmail.com>
2023-05-02 16:20:54 +0530

    URL issue resolved

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -7,8 +7,8 @@ const BottomMenu = () => {
     return <>
 
         <ul className="menu bottom-menu bg-secondary d-flex">
-            <li><Link title="Create Contact" className="bg-primary bg-gradient text-white" to="/create-contact"><i className="fas fa-plus"></i></Link></li>
-            <li><Link title="View Contacts" className="bg-primary bg-gradient text-white" to="/view-contacts"><i className="fas fa-list"></i></Link></li>
+            <li><Link title="Create Contact" className="bg-primary bg-gradient text-white" to="/create-contact">Add New</Link></li>
+            <li><Link title="View Contacts" className="bg-primary bg-gradient text-white" to="/view-contacts">View Contacts</Link></li>
         </ul>
         <Routes>
             <Route exact path="/" element={<ViewAllContacts />}>View Contacts</Route>
